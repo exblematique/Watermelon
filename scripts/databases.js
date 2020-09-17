101985d22d0a5920af78c453c53464a5d3f6d7f4
exblematique <yoann.chappaz@outlook.com>
2020-09-17 19:45:56 +0200

    Initial Commit

---
+++ scripts/databases.js
@@ -0,0 +1,15 @@
+let skills = [
+    {
+        "title": "Préproduction",
+        "desc": "Avec notre équipe spécialisée en Préproduction, nous nous engageons à fournir un résultat de la plus haute qualité à tous nos clients. Quelle que soit la taille de votre projet, nous sommes ici pour vous aider à le concrétiser.",
+        "img": "preproduction.webp"
+    },{
+        "title": "Développement créatif",
+        "desc": "Le développement créatif est l'une des étapes les plus complexes de tout projet - nous sommes donc fiers de la compter parmi nos spécialités. Avec notre équipe d’experts, vous avez la garantie que votre contenu visuel sera exceptionnel.",
+        "img": "development.webp"
+    },{
+        "title": "Postproduction",
+        "desc": "",
+        "img": "postproduction.webp"
+    }
+];

56ec8e7d23da8812d75e9cd20f037831b181a070
exblematique <yoann.chappaz@outlook.com>
2021-01-11 21:38:52 +0100

    All packages updated and work

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,41 +1,31 @@
-module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'Watermelon',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Watermelon est une association de loi 1901 qui réalise vos projets de la partie préproduction à la partie post-production.' },
-      { hid: 'keyword', name: 'keyword', content: 'AUDIOVISUEL, PRODUCTION, REALISATION, ASSOCIATION' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', type: 'text/css', media: 'screen',  href: '/default.css'}
-    ]
-  },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
+export default {
+  buildModules: [
+    '@nuxtjs/eslint-module'
+  ],
+
     /*
-    ** Run ESLint on save
+    ** Headers of the page
     */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
+    head: {
+      title: 'Watermelon',
+      meta: [
+        {charset: 'utf-8'},
+        {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Watermelon est une association de loi 1901 qui réalise vos projets de la partie préproduction à la partie post-production.'
+        },
+        {hid: 'keyword', name: 'keyword', content: 'AUDIOVISUEL, PRODUCTION, REALISATION, ASSOCIATION'}
+      ],
+      link: [
+        {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+        {rel: 'stylesheet', type: 'text/css', media: 'screen', href: '/default.css'}
+      ]
+    },
+    /*
+    ** Customize the progress bar color
+    */
+    loading: {color: '#3B8070'}
 }
 

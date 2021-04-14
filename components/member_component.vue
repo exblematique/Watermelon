1a871a3100922186146a6ac2ddb6cae483ac9e27
Yoann Chappaz <yoann.chappaz@outlook.com>
2021-04-14 18:03:39 +0200

    Create template to display a member of a team Implement member_component in team.vue

---
+++ components/member_component.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>{{ member.name }}</h2>
+    <h3
+      v-for="(role, index) in member.roles"
+      :key="index"
+    >
+      {{ role }}
+    </h3>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'member',
+  props: {
+    member: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
+<style scoped>
+/*
+  TODO Change image to custom url
+  style="background-image: url('team/{{ member.img }}');"
+ */
+div {
+  background-image: url("static/team/xavier.webp");
+}
+</style>
--- pages/team.vue
+++ pages/team.vue
@@ -1,10 +1,35 @@
 <template>
-  <h1>Ceci est l'équipe</h1>
+  <div id="team">
+    <h1>Ceci est l'équipe</h1>
+    <member_component
+      v-for="(member, index) in team"
+      :key="index"
+      :member="member"
+    />
+  </div>
 </template>
 
 <script>
+import member_component from "@/components/member_component";
 export default {
-  name: "Team"
+  name: "Team",
+  components: {member_component},
+  data : () => {
+    return {
+      // TODO: move team information in .json file
+      team: [
+        {
+          name: "Bouquet Xavier",
+          roles: ["Président de l'association", "Monteur"],
+          img: "xavier.webp"
+        },{
+          name: "Signaire Caroline",
+          roles: ["Trésorière", "Monteuse"],
+          img: "caroline.webp"
+        }
+      ]
+    }
+  }
 }
 </script>
 

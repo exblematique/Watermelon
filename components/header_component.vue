8ed3ffcc53c551dab50d86811a56a2f00a324b41
Yoann Chappaz <yoann.chappaz@outlook.com>
2021-04-26 17:30:25 +0200

    Adding class currentRoute Needs to check a condition to display when image is ready (in member.vue) Bug when double press a button to change page Some tests for event (need to clear)

--- components/header_component.vue
+++ components/header_component.vue
@@ -1,39 +1,94 @@
 <template>
   <header>
-    <img
-      class="logo"
-      src="/logo.webp"
-      alt="Logo de l'association Watermelon"
-    >
-    <div class="links">
-      <NuxtLink
-        to="/"
+    <div class="flex underline">
+      <img
+        class="logo"
+        src="/logo.webp"
+        alt="Logo de l'association Watermelon"
       >
-        Accueil
-      </NuxtLink>
-      <NuxtLink
-        to="/team"
-      >
-        Équipe
-      </NuxtLink>
-      <NuxtLink
-        to="/contact"
-      >
-        Contact
-      </NuxtLink>
+      <div class="flex links">
+        <div
+          v-for="(link, index) in links"
+          :key="index"
+          class="flexItem"
+          :class="{currentRoute : currentLink(link.to)}"
+        >
+          <NuxtLink :to="link.to">
+            {{ link.text }}
+          </NuxtLink>
+        </div>
+      </div>
     </div>
   </header>
 </template>
+
+<script>
+export default {
+  data() { return {
+    links: [
+      {
+        to: "/",
+        text: "Accueil"
+      },{
+        to: "/abilities",
+        text: "Compétences"
+      },{
+        to: "/team",
+        text: "Équipe"
+      },{
+        to: "/contact",
+        text: "Contact"
+      }
+    ]
+  }},
+  methods: {
+    currentLink(link) {
+      if (link === '/')
+        return this.$route.name === "index";
+      else
+        return "/"+this.$route.name === link;
+    }
+  }
+}
+</script>
 
 <style scoped>
 /* Particular CSS when the link forward to the current page */
 .nuxt-link-exact-active{
   cursor: default;
   pointer-events: none;
+}
+div > .nuxt-link-exact-active{
   background-color: red;
 }
-div {
-  height: auto;
+
+.flexItem {
+  height: 100%;
   width: auto;
+  margin: auto 20px;
+  font-size: 20px;
+}
+
+.flex{
+  height: 100%;
+  display: inline-flex;
+  flex-wrap: wrap;
+  align-items: stretch;
+}
+.links{
+  width: auto;
+  justify-content: space-evenly;
+}
+.underline{
+  width: 100%;
+  justify-content: space-between;
+  border-bottom: black solid;
+}
+
+/*
+ * HEADER
+ */
+header {
+  padding: 30px 60px;
 }
 </style>
